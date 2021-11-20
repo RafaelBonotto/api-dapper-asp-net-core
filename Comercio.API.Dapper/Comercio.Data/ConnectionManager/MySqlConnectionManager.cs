e4353d93daafb9532585cbedc4fbd43cfd08b2a0
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-19 23:46:36 -0300

    Create MySqlConnectionManager.cs

---
+++ Comercio.API.Dapper/Comercio.Data/ConnectionManager/MySqlConnectionManager.cs
@@ -0,0 +1,32 @@
+﻿using MySqlConnector;
+using System;
+using System.Threading.Tasks;
+
+namespace Comercio.Data.ConnectionManager
+{
+    public class MySqlConnectionManager : IMySqlConnectionManager
+    {
+        private readonly string _connectionString;
+
+        public MySqlConnectionManager()
+        {
+            _connectionString = "";// RepositoryBase.stringConnection; 
+        }
+
+        public async Task<MySqlConnection> GetConnectionAsync()
+        {            
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(_connectionString);                
+                                                                        
+                await connection.OpenAsync();
+
+                return connection;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
