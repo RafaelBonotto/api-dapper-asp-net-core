4127905b0324b36ec325acaa9cd837e765539fd2
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-20 02:22:10 -0300

    Create ProdutoController

---
+++ Comercio.API.Dapper/Comercio.API/Controllers/ProdutoController.cs
@@ -0,0 +1,24 @@
+﻿using Comercio.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Comercio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProdutoService _produtoService;
+
+        public ProdutoController(IProdutoService produtoService)
+        {
+            _produtoService = produtoService;
+        }
+
+        [HttpGet("listar")]
+        public async Task<IActionResult> ListarProdutos()
+        {
+            return Ok(await _produtoService.ObterProdutos());
+        }
+    }
+}
