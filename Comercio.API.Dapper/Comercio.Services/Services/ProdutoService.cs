08db210c92c69bc6acfec9f493c6d9ec3434ce54
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-20 02:20:20 -0300

    Create ProdutoService.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Services/ProdutoService.cs
@@ -0,0 +1,29 @@
+﻿using Comercio.Domain.Interfaces;
+using Comercio.Services.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Comercio.Services.Services
+{
+    public class ProdutoService : IProdutoService
+    {
+        private readonly IProdutoRepository _repository;
+
+        public ProdutoService(IProdutoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<dynamic> ObterProdutos()
+        {
+            try
+            {
+                return await _repository.ListarProdutos();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
