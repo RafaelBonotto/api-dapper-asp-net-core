fdc053d91598256e83aed8f85f486908863b8c13
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-20 23:54:53 -0300

    Create SetorService.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Services/SetorService.cs
@@ -0,0 +1,41 @@
+﻿using Comercio.Domain.Entities;
+using Comercio.Domain.Interfaces;
+using Comercio.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace Comercio.Services.Services
+{
+    public class SetorService : ISetorService
+    {
+        private readonly ISetorRepository _setorRepository;
+
+        public SetorService(ISetorRepository setorRepository)
+        {
+            _setorRepository = setorRepository;
+        }
+
+        public Task<dynamic> ObterSetor()
+        {
+            try
+            {
+                return _setorRepository.ObterSetor();
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        public Task<Setor> ObterSetorPorId(int id)
+        {
+            try
+            {
+                return _setorRepository.ObterSetorPorId(id);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
