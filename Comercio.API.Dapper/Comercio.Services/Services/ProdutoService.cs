28029bccb5fa9e67522ea2c5efcf9bc55e123d95
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-21 00:31:07 -0300

    Update ProdutoService.cs

--- Comercio.API.Dapper/Comercio.Services/Services/ProdutoService.cs
+++ Comercio.API.Dapper/Comercio.Services/Services/ProdutoService.cs
@@ -1,6 +1,8 @@
-﻿using Comercio.Domain.Interfaces;
+﻿using Comercio.Domain.Entities;
+using Comercio.Domain.Interfaces;
 using Comercio.Services.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Comercio.Services.Services
@@ -14,11 +16,23 @@ namespace Comercio.Services.Services
             _repository = repository;
         }
 
-        public async Task<dynamic> ObterProdutos()
+        public async Task<List<Produto>> ObterProdutos()
         {
             try
             {
                 return await _repository.ListarProdutos();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<Produto> ObterPorId(int id)
+        {
+            try
+            {
+                return await _repository.ObterPorId(id);
             }
             catch (Exception)
             {
