1100939ff7d0e5c3bb43cbc8654fb3b576fd343c
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-20 23:36:10 -0300

    Update Setor.cs

--- Comercio.API.Dapper/Comercio.Domain/Entities/Setor.cs
+++ Comercio.API.Dapper/Comercio.Domain/Entities/Setor.cs
@@ -1,6 +1,13 @@
-﻿namespace Comercio.Domain.Entities
+﻿using System;
+
+namespace Comercio.Domain.Entities
 {
     public class Setor
     {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public sbyte Ativo { get; set; } 
+        public DateTime Data_criacao { get; set; }
+        public DateTime Data_alteracao { get; set; }
     }
 }
