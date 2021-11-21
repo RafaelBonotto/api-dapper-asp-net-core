dff13a861f66f5f26df6039922286df2b6d5e311
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-20 23:54:48 -0300

    Create ISetorService.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Interfaces/ISetorService.cs
@@ -0,0 +1,11 @@
+﻿using Comercio.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Comercio.Services.Interfaces
+{
+    public interface ISetorService
+    {
+        Task<dynamic> ObterSetor();
+        Task<Setor> ObterSetorPorId(int id);
+    }
+}
