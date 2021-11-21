5d2e5811b31e20fb01deb9f4f4499d0b0c988166
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-20 23:50:01 -0300

    Create ISetorRepository.cs

---
+++ Comercio.API.Dapper/Comercio.Domain/Interfaces/ISetorRepository.cs
@@ -0,0 +1,11 @@
+﻿using Comercio.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Comercio.Domain.Interfaces
+{
+    public interface ISetorRepository
+    {
+        Task<dynamic> ObterSetor();
+        Task<Setor> ObterSetorPorId(int id);
+    }
+}
