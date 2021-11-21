5f1c8db5edf61d919a14e687d7360681f67eb901
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-21 00:31:41 -0300

    Create SetorController.cs

---
+++ Comercio.API.Dapper/Comercio.API/Controllers/SetorController.cs
@@ -0,0 +1,30 @@
+﻿using Comercio.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Comercio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SetorController : ControllerBase
+    {
+        private readonly ISetorService _setorService;
+
+        public SetorController(ISetorService setorService)
+        {
+            _setorService = setorService;
+        }
+
+        [HttpGet("listar")]
+        public async Task<IActionResult> ObterSetor()
+        {
+            return Ok(await _setorService.ObterSetor());
+        }
+
+        [HttpGet("id/{id}")]
+        public async Task<IActionResult> ObterSetorPorId(int id)
+        {
+            return Ok(await _setorService.ObterSetorPorId(id));
+        }
+    }
+}
