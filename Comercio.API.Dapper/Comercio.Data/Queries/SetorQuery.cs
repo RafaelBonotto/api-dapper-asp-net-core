dfe99b9ab2037037796eb32d3090bd7cfc65050e
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-20 23:36:04 -0300

    Create SetorQuery.cs

---
+++ Comercio.API.Dapper/Comercio.Data/Queries/SetorQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Comercio.Data.Queries
+{
+    public class SetorQuery
+    {
+        public const string SELECT_SETOR = "SELECT * FROM tb_setor WHERE ativo = 1";
+
+        public const string SELECT_SETOR_POR_ID = "SELECT * FROM tb_setor WHERE id = @Id";
+    }
+}
