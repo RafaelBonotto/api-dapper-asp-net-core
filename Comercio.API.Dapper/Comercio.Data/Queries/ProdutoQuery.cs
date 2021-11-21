63cdacd9039bf6aabf06e196f3e7784bcae25bb0
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-21 00:31:34 -0300

    Update ProdutoQuery.cs

--- Comercio.API.Dapper/Comercio.Data/Queries/ProdutoQuery.cs
+++ Comercio.API.Dapper/Comercio.Data/Queries/ProdutoQuery.cs
@@ -3,5 +3,7 @@
     public class ProdutoQuery
     {
         public const string  SELECT_PRODUTOS = "SELECT * FROM tb_produto WHERE ativo = 1";
+
+        public const string  SELECT_PRODUTO_POR_ID = "SELECT * FROM tb_produto WHERE id = @Id";
     }
 }
