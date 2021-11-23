40c964c3003ccddeb21e7ea1ef6e68f738c8f2ba
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-22 23:40:01 -0300

    Update Produto.cs

--- Comercio.API.Dapper/Comercio.Domain/Entities/Produto.cs
+++ Comercio.API.Dapper/Comercio.Domain/Entities/Produto.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Comercio.Domain.Entities
 {
     public class Produto
     {
         public long Id { get; set; }
+
+        [Required(ErrorMessage ="Código obrigatório")]
         public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Descrição obrigatório")]
         public string Descricao { get; set; }
         public string Setor { get; set; }
+
+        [Required(ErrorMessage ="Preço de custo obrigatório")]
         public decimal Preco_custo { get; set; }
+
+        [Required(ErrorMessage ="Preço de venda obrigatório")]
         public decimal Preco_venda { get; set; }
         public DateTime Data_fabricacao { get; set; }
         public DateTime Data_validade { get; set; }
