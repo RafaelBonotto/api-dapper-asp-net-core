50e968545a96f8c3ad27c78fe6722283fecb7d98
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-23 23:44:42 -0300

    Create ProdutoResponse.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Response/ProdutoResponse.cs
@@ -0,0 +1,11 @@
+﻿using Comercio.Domain.Base;
+using Comercio.Domain.Entities;
+
+namespace Comercio.Services.Response
+{
+    public class ProdutoResponse : StatusResult
+    {      
+        public Produto Produto { get; set; }
+    }
+
+}
