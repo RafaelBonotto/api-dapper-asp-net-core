cd6892129c263fc2ba0c28907df1f75d8a4df0f4
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-23 23:44:46 -0300

    Create ProdutoRequest.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Request/ProdutoRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Services.Request
+{
+    public class ProdutoRequest
+    {
+
+        [Required(ErrorMessage = "Código obrigatório")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Descrição obrigatório")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Preço de custo obrigatório")]
+        public decimal Preco_custo { get; set; }
+
+        [Required(ErrorMessage = "Preço de venda obrigatório")]
+        public decimal Preco_venda { get; set; }       
+        public long Setor_id { get; set; }
+
+    }
+}
