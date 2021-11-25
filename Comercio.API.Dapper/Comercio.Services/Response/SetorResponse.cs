48adbb1159992b58ecc7f3d3b87962d49edeaf2e
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-24 23:44:48 -0300

    Create SetorResponse.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Response/SetorResponse.cs
@@ -0,0 +1,10 @@
+﻿using Comercio.Domain.Base;
+using Comercio.Domain.Entities;
+
+namespace Comercio.Services.Response
+{
+    public class SetorResponse : StatusResult
+    {
+        public Setor Setor{ get; set; }
+    }
+}
