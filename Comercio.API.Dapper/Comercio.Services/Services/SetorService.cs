df1c07b2bdb1699c8e7fe825da14f967fa0269ba
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-24 23:45:01 -0300

    Update SetorService.cs

--- Comercio.API.Dapper/Comercio.Services/Services/SetorService.cs
+++ Comercio.API.Dapper/Comercio.Services/Services/SetorService.cs
@@ -1,6 +1,9 @@
 ﻿using Comercio.Domain.Entities;
 using Comercio.Domain.Interfaces;
 using Comercio.Services.Interfaces;
+using Comercio.Services.Request;
+using Comercio.Services.Response;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -13,6 +16,27 @@ namespace Comercio.Services.Services
         public SetorService(ISetorRepository setorRepository)
         {
             _setorRepository = setorRepository;
+        }
+
+        public async Task<SetorResponse> InserirSetor(SetorRequest setor)
+        {
+            var ret = new SetorResponse();
+            try
+            {
+                var novoSetor = new Setor(){ Descricao = setor.Descricao };
+
+                await _setorRepository.InserirSetor(novoSetor);
+                ret.Setor = novoSetor;
+                ret.Sucesso = true;
+                ret.Mensagem = "Setor inserido com sucesso";
+                return ret;
+            }
+            catch (Exception error)
+            {
+                ret.Sucesso = false;
+                ret.Mensagem = error.Message;
+                return ret;
+            }
         }
 
         public Task<List<Setor>> ObterSetor()
