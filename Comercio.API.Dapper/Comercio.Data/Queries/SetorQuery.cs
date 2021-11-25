8a3ab2a76c4ebe2804bb8c8df98344456a40d7e3
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-24 23:44:24 -0300

    Update SetorQuery.cs

--- Comercio.API.Dapper/Comercio.Data/Queries/SetorQuery.cs
+++ Comercio.API.Dapper/Comercio.Data/Queries/SetorQuery.cs
@@ -1,9 +1,19 @@
-﻿namespace Comercio.Data.Queries
-{
-    public class SetorQuery
-    {
-        public const string SELECT_SETOR = "SELECT * FROM tb_setor WHERE ativo = 1";
+﻿using Comercio.Domain.Entities;
+using Dapper;
+using System.Data;
 
-        public const string SELECT_SETOR_POR_ID = "SELECT * FROM tb_setor WHERE id = @Id";
+namespace Comercio.Data.Queries
+{
+    public static class SetorQuery
+    {
+        public const string SELECT_SETOR = "SELECT * FROM comercioDB.tb_setor WHERE ativo = 1";
+
+        public const string SELECT_SETOR_POR_ID = "SELECT * FROM comercioDB.tb_setor WHERE id = @Id";
+
+        public static string RetornaQueryInsertSetor(Setor setor)
+        {
+            return "INSERT INTO comercioDB.tb_setor(descricao, ativo, data_criacao, data_alteracao) " +
+                   $"VALUES('{setor.Descricao}', 1, now(), now())";
+        }
     }
 }
