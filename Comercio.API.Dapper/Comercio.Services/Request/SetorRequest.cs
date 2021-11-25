905b756fed47ac2338cbf4fb7487b591697fbb44
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-24 23:44:52 -0300

    Create SetorRequest.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Request/SetorRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Services.Request
+{
+    public class SetorRequest
+    {
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Descricao { get; set; }
+    }
+}
