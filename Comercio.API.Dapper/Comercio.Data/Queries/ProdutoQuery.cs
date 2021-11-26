958e367dc4436e7e7209900bef77f79bcae84675
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-25 23:45:27 -0300

    Update ProdutoQuery.cs

--- Comercio.API.Dapper/Comercio.Data/Queries/ProdutoQuery.cs
+++ Comercio.API.Dapper/Comercio.Data/Queries/ProdutoQuery.cs
@@ -10,9 +10,13 @@ namespace Comercio.Data.Queries
 
         public static string RetornaQueryInsertProduto(Produto produto)
         {
-            return  "INSERT INTO comercioDB.tb_produto " +
-                    "(codigo, descricao, preco_custo, preco_venda, data_fabricacao, data_validade, ativo, data_criacao, data_alteracao, setor_id)" +
-                    $"VALUES ('{produto.Codigo}', '{produto.Descricao}', {produto.Preco_custo}, {produto.Preco_venda}, now(), now(), 1, now(), now(), {produto.Setor_id})";
+            var query = "START TRANSACTION;" +
+                        "INSERT INTO comercioDB.tb_produto " +
+                        "(codigo, descricao, preco_custo, preco_venda, data_fabricacao, data_validade, ativo, data_criacao, data_alteracao, setor_id)" +
+                        $"VALUES ('{produto.Codigo}', '{produto.Descricao}', {produto.Preco_custo}, {produto.Preco_venda}, now(), now(), 1, now(), now(), {produto.Setor_id});" +
+                        "SELECT LAST_INSERT_ID();" +
+                        "COMMIT;";
+            return query;
         }
     }
 }
