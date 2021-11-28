4be111e22732c19bd94cedc73ea9a7a57aeebf91
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-27 23:52:12 -0300

    Update SetorQuery.cs

--- Comercio.API.Dapper/Comercio.Data/Queries/SetorQuery.cs
+++ Comercio.API.Dapper/Comercio.Data/Queries/SetorQuery.cs
@@ -10,12 +10,28 @@ namespace Comercio.Data.Queries
 
         public const string SELECT_SETOR_POR_ID = "SELECT * FROM comercioDB.tb_setor WHERE id = @Id";
 
+        public const string SELECT_SETOR_POR_DESCRICAO = "SELECT * FROM comercioDB.tb_setor WHERE descricao = @Descricao";
+
+        public const string DELETE_SETOR = "UPDATE comercioDB.tb_setor SET ativo = 0 WHERE id = @Id;";
+
         public static string RetornaQueryInsertSetor(Setor setor)
         {
             var query= "START TRANSACTION;" +
                         "INSERT INTO comercioDB.tb_setor(descricao, ativo, data_criacao, data_alteracao)" +
                         $"VALUES('{setor.Descricao}', 1, now(), now());" +
                         "SELECT LAST_INSERT_ID();" +
+                        "COMMIT;";
+            return query;
+        }
+
+        public static string RetornaQueryUpdateSetor(Setor setor)
+        {
+            var query = "START TRANSACTION;" +
+                        $"UPDATE comercioDB.tb_setor SET " +
+                        $"descricao = '{setor.Descricao}' , " +                       
+                        "ativo = 1 , " +
+                        "data_alteracao = now() " +
+                        $"WHERE id = {setor.Id};" +
                         "COMMIT;";
             return query;
         }
