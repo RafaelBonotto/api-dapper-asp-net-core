2e1888a9a2f19b31907baeaf82dfa3d8e986fa6e
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-28 23:36:58 -0300

    Create ListaSetorResponse.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Response/ListaSetorResponse.cs
@@ -0,0 +1,15 @@
+﻿using Comercio.Domain.Base;
+using Comercio.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Comercio.Services.Response
+{
+    public class ListaSetorResponse : StatusResult
+    {
+        public ListaSetorResponse()
+        {
+            Setores = new List<Setor>();
+        }
+        public List<Setor> Setores { get; set; }
+    }
+}
