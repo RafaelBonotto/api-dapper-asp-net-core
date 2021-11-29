4c2ff9a5cb484941cde42253195b0c46ebda268f
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-11-28 23:36:53 -0300

    Create ListaProdutoResponse.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Response/ListaProdutoResponse.cs
@@ -0,0 +1,15 @@
+﻿using Comercio.Domain.Base;
+using Comercio.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Comercio.Services.Response
+{
+    public class ListaProdutoResponse : StatusResult
+    {
+        public ListaProdutoResponse()
+        {
+            Produtos = new List<Produto>();
+        }
+        public List<Produto> Produtos { get; set; }
+    }
+}
