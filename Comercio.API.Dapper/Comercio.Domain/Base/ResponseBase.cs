c910f1bba78156abf68cf188297cf932b93927de
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-04 01:24:57 -0300

    Create ResponseBase.cs

---
+++ Comercio.API.Dapper/Comercio.Domain/Base/ResponseBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Comercio.Domain.Base
+{
+    public class ResponseBase<T>
+    {
+        public ResponseBase(T data, List<string> errors)
+        {
+            Data = data;
+            Errors = errors;
+        }
+
+        public ResponseBase(T data)
+        {
+            Data = data;
+        }
+
+        public ResponseBase(List<string> errors)
+        {
+            Errors = errors;
+        }
+
+        public ResponseBase(string error)
+        {
+            Errors.Add(error);
+        }
+
+        public T Data { get; private set; }
+        public List<string> Errors { get; private set; } = new();
+    }
+}
