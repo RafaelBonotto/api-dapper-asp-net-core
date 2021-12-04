a0668922f822a179fe3ede0598a7010a149d1361
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-04 01:26:10 -0300

    Update SetorRequest.cs

--- Comercio.API.Dapper/Comercio.Services/Request/SetorRequest.cs
+++ Comercio.API.Dapper/Comercio.Services/Request/SetorRequest.cs
@@ -5,6 +5,7 @@ namespace Comercio.Services.Request
     public class SetorRequest
     {
         [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres")]
         public string Descricao { get; set; }
     }
 }
