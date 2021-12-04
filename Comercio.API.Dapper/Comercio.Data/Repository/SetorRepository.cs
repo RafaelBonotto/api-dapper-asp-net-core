a3477521b2c94c2ad6a275593a37f7ef7978dc5f
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-04 01:25:57 -0300

    Update SetorRepository.cs

--- Comercio.API.Dapper/Comercio.Data/Repository/SetorRepository.cs
+++ Comercio.API.Dapper/Comercio.Data/Repository/SetorRepository.cs
@@ -5,7 +5,6 @@ using Comercio.Domain.Interfaces;
 using Dapper;
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,37 +20,31 @@ namespace Comercio.Data.Repository
         }
 
         public async Task<List<Setor>> ObterSetor()
-        {
-            List<Setor> setorBanco;
+        {            
             try
             {
-                using (var connection = await _connection.GetConnectionAsync())
-                {
-                    setorBanco = connection.Query<Setor>(SetorQuery.SELECT_SETOR).ToList();
-                }
-
+                List<Setor> setorBanco;
+                using var connection = await _connection.GetConnectionAsync();
+                setorBanco = connection.Query<Setor>(SetorQuery.SELECT_SETOR).ToList();
                 return setorBanco;
             }
             catch (System.Exception)
             {
                 throw;
-            }
-           
+            }           
         }
 
         public async Task<Setor> InserirSetor(Setor setor)
         {
             try
             {
-                using (var connection = await _connection.GetConnectionAsync())
-                {
-                    var checkDescricao = await connection.QueryFirstOrDefaultAsync<Setor>(SetorQuery.SELECT_SETOR_POR_DESCRICAO, new { Descricao = setor.Descricao });
-                    if (checkDescricao != null)
-                        throw new Exception("Não foi possível inserir o setor com essa descrição");
+                using var connection = await _connection.GetConnectionAsync();
+                var checkDescricao = await connection.QueryFirstOrDefaultAsync<Setor>(SetorQuery.SELECT_SETOR_POR_DESCRICAO, new { Descricao = setor.Descricao });
+                if (checkDescricao != null)
+                    throw new Exception("Não foi possível inserir o setor com essa descrição");
+                var setorId = await connection.ExecuteScalarAsync<long>(SetorQuery.RetornaQueryInsertSetor(setor));
 
-                    var setorId = await connection.ExecuteScalarAsync<long>(SetorQuery.RetornaQueryInsertSetor(setor));
-                    return await this.ObterSetorPorId(setorId);
-                }                
+                return await this.ObterSetorPorId(setorId);
             }
             catch (System.Exception)
             {
@@ -63,15 +56,9 @@ namespace Comercio.Data.Repository
         {
             try
             {
-                using (var connection = await _connection.GetConnectionAsync())
-                {
-                    var checkDescricao = await connection.QueryFirstOrDefaultAsync<Setor>(SetorQuery.SELECT_SETOR_POR_DESCRICAO, new { Descricao = setor.Descricao });
-                    if (checkDescricao != null)
-                        throw new Exception("Não foi possível atualizar o setor com essa descrição");
-
-                    var setorId = await connection.ExecuteScalarAsync<long>(SetorQuery.RetornaQueryUpdateSetor(setor));
-                    return await this.ObterSetorPorId(setorId);
-                }
+                using var connection = await _connection.GetConnectionAsync();
+                await connection.QueryAsync(SetorQuery.RetornaQueryUpdateSetor(setor));
+                return await this.ObterSetorPorId(setor.Id);
             }
             catch (System.Exception)
             {
@@ -81,14 +68,12 @@ namespace Comercio.Data.Repository
 
         public async Task<Setor> ObterSetorPorId(long id)
         {
-            Setor setor;
             try
             {
-                using (var connection = await _connection.GetConnectionAsync())
-                {
-                    setor = await connection.QueryFirstOrDefaultAsync<Setor>(SetorQuery.SELECT_SETOR_POR_ID, new { Id = id });
-                }
-
+                using var connection = await _connection.GetConnectionAsync();
+                var setor = await connection.QueryFirstOrDefaultAsync<Setor>(SetorQuery.SELECT_SETOR_POR_ID, new { Id = id });
+                if (setor == null)
+                    throw new Exception("Setor não encontrado no sistema");
                 return setor;
             }
             catch (System.Exception)
@@ -101,10 +86,11 @@ namespace Comercio.Data.Repository
         {
             try
             {
-                using (var connection = await _connection.GetConnectionAsync())
-                {
-                    return await connection.QueryAsync<Setor>(SetorQuery.DELETE_SETOR, new { Id = setorId }) != null;
-                }
+                using var connection = await _connection.GetConnectionAsync();
+                var setorExiste = await connection.QueryFirstOrDefaultAsync<Setor>(SetorQuery.SELECT_SETOR_POR_ID, new { Id = setorId }) != null;
+                if (!setorExiste)
+                    throw new Exception("Setor não encontrado no sistema");
+                return await connection.QueryAsync<Setor>(SetorQuery.DELETE_SETOR, new { Id = setorId }) != null;
             }
             catch (System.Exception)
             {
