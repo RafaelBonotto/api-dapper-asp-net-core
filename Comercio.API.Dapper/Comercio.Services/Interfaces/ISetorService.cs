a2bb293eec61abe2d2c41474f916e197cfdbc589
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-04 01:26:19 -0300

    Update ISetorService.cs

--- Comercio.API.Dapper/Comercio.Services/Interfaces/ISetorService.cs
+++ Comercio.API.Dapper/Comercio.Services/Interfaces/ISetorService.cs
@@ -1,15 +1,17 @@
-﻿using Comercio.Services.Request;
-using Comercio.Services.Response;
+﻿using Comercio.Domain.Base;
+using Comercio.Domain.Entities;
+using Comercio.Services.Request;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Comercio.Services.Interfaces
 {
     public interface ISetorService
     {
-        Task<ListaSetorResponse> ObterSetor();
-        Task<SetorResponse> ObterSetorPorId(long id);
-        Task<SetorResponse> InserirSetor(SetorRequest setor);
-        Task<SetorResponse> ExcluirSetor(long setorId);
-        Task<SetorResponse> AtualizarSetor(long setorId, SetorRequest setor);
+        Task<ResponseBase<List<Setor>>> ObterSetor();
+        Task<ResponseBase<Setor>> ObterSetorPorId(long id);
+        Task<ResponseBase<Setor>> InserirSetor(SetorRequest setor);
+        Task<ResponseBase<bool>> ExcluirSetor(long setorId);
+        Task<ResponseBase<Setor>> AtualizarSetor(long setorId, SetorRequest setor);
     }
 }
