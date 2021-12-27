e701307d7b9160bf29920d1475776dbb51962ad5
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-27 17:39:43 -0300

    Create Usuario.cs

---
+++ Comercio.API.Dapper/Comercio.Domain/Entities/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Comercio.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public List<Permissao> Permissoes { get; set; } = new();
+    }
+}
