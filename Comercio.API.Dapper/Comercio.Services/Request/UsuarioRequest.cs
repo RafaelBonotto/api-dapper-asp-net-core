3ea0176057eaa1016182566fdfe39d2e828141b1
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-27 17:39:08 -0300

    Create UsuarioRequest.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Request/UsuarioRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comercio.Services.Request
+{
+    public class UsuarioRequest
+    {
+        [Required(ErrorMessage ="Campo obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+    }
+}
