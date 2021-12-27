13577fec11545aae74de056092fd5b938fa2a328
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2021-12-27 17:39:16 -0300

    Create LoginRequest.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Request/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Services.Request
+{
+    public class LoginRequest
+    {
+        [Required(ErrorMessage =("Informe o E-mail"))]
+        [EmailAddress(ErrorMessage =("E-mail inválido"))]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage =("Informe a senha"))]
+        public string Senha { get; set; }
+    }
+}
