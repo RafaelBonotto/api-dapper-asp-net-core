d2735e7b38c580877372e0bc6f5b2cea9fb3fb4a
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-03 17:47:10 -0300

    Create IPermissaoRepository.cs

---
+++ Comercio.API.Dapper/Comercio.Domain/Interfaces/IPermissaoRepository.cs
@@ -0,0 +1,11 @@
+﻿using Comercio.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Comercio.Domain.Interfaces
+{
+    public interface IPermissaoRepository
+    {
+        Task<List<Permissao>> ObterPermissaoUsuario(long idUsuario);
+    }
+}
