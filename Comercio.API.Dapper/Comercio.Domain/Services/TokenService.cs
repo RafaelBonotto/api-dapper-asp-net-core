31d4a804031a43f2ef20fb66e2337cb5c6701b09
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-03 17:46:50 -0300

    Create TokenService.cs

---
+++ Comercio.API.Dapper/Comercio.Domain/Services/TokenService.cs
@@ -0,0 +1,35 @@
+﻿using Comercio.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Comercio.Domain.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(Usuario usuario)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Configurationn.JwtKey);
+            //var claims = usuario.GetClaims();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] 
+                {
+                    new (ClaimTypes.Name, "Nome do usuario"),//usuario.Nome),
+                    new (ClaimTypes.Role, "admin"),
+                    new ("fruta", "banana")                
+                }),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
