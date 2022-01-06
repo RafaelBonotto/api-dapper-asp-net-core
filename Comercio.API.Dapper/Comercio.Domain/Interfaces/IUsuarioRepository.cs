7c499bacb4571f91f0482ff5b1f0adbd484ec432
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-06 18:05:54 -0300

    Create IUsuarioRepository.cs

---
+++ Comercio.API.Dapper/Comercio.Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using Comercio.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Comercio.Domain.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario> ObterUsuarioPorEmail(string email);
+        Task<Usuario> Registrar();
+    }
+}
