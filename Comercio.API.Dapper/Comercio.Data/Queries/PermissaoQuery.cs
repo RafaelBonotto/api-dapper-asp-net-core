b960b7286a8c930b2c9758c5ed7095292793b144
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-06 18:06:13 -0300

    Create PermissaoQuery.cs

---
+++ Comercio.API.Dapper/Comercio.Data/Queries/PermissaoQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace Comercio.Data.Queries
+{
+    public static class PermissaoQuery
+    {
+        public const string SELECT_PERMISSAO_USUARIO = "SELECT * id_descricao comercioDB.tb_usuario_permissao WHERE id_usuario = @IdUsuario";
+
+    }
+}
