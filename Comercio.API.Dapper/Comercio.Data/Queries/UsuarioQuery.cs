6c716f0d1e0723bb42e39638b1f2f0043c149aa7
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-06 18:06:19 -0300

    Create UsuarioQuery.cs

---
+++ Comercio.API.Dapper/Comercio.Data/Queries/UsuarioQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Comercio.Data.Queries
+{
+    public static class UsuarioQuery
+    {
+        public const string SELECT_USUARIO = "SELECT * FROM comercioDB.tb_usuario WHERE email = @Email";
+    }
+}
