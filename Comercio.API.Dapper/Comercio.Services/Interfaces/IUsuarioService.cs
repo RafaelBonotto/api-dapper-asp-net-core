071916800052a9b8f997b15e1209c8284c4227a6
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-08 22:54:32 -0300

    Create IUsuarioService.cs

---
+++ Comercio.API.Dapper/Comercio.Services/Interfaces/IUsuarioService.cs
@@ -0,0 +1,14 @@
+﻿using Comercio.Domain.Base;
+using Comercio.Domain.Entities;
+using Comercio.Services.Request;
+using System.Threading.Tasks;
+
+namespace Comercio.Services.Interfaces
+{
+    public interface IUsuarioService
+    {        
+        Task<ResponseBase<Usuario>> Regsitrar(UsuarioRequest usuario);
+        //Task<ResponseBase<Usuario>> Login();
+        Task<ResponseBase<string>> Login(LoginRequest loginRequest);
+    }
+}
