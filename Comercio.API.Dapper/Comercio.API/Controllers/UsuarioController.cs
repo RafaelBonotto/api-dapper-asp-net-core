b8f0cd4f1f3261966d75d6c4bcb7fc76626cfcf6
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-08 22:54:49 -0300

    Create UsuarioController.cs

---
+++ Comercio.API.Dapper/Comercio.API/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using Comercio.Domain.Base;
+using Comercio.Domain.Entities;
+using Comercio.Domain.Extensions;
+using Comercio.Domain.Services;
+using Comercio.Services.Interfaces;
+using Comercio.Services.Request;
+using Comercio.Services.Services;
+using Microsoft.AspNetCore.Mvc;
+using SecureIdentity.Password;
+using System.Threading.Tasks;
+
+namespace Comercio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UsuarioRequest usuarioRequest, [FromServices] IUsuarioService usuarioService)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResponseBase<string>(ModelState.GetErrors()));            
+
+            return Ok(await usuarioService.Regsitrar(usuarioRequest));
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest, [FromServices] IUsuarioService usuarioService)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResponseBase<string>(ModelState.GetErrors()));
+
+            return Ok(await usuarioService.Login(loginRequest));
+        }
+    }
+}
